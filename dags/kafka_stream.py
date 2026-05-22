ea1076bc258d29ba4cb404914992324f15a9f44c
wakh3e <223608238@edu.vut.ac.za>
2026-05-22 05:28:10 +0000

    l

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -7,7 +7,7 @@ import time
 from kafka import KafkaProducer
 
 
-def stream_data_from_api():
+def ingest_data_api():
     # Placeholder for the actual streaming logic
     print("Streaming data from API...")
 
@@ -24,6 +24,35 @@ def format_data(res):
     data['last_name'] = res['name']['last']
     data['address'] = f"{location['street']['number']} {location['street']['name']}, {location['city']}"
     return data
+    
+#converts dictionary to json binary object readable for kafka
+def serializer(data):
+    return json.dumps(data).encode('utf-8')
+
+
+def stream_data(data):
+    server = 'broker:2902' # Kafka broker address within the Docker network
+    
+    #kafka producer to stream data
+    producer = KafkaProducer(bootstrap_servers=[server], max_block_ms=5000)
+    curr_time = time.time()
+
+    # Streaming Loop: Simulates a persistent high-velocity stream for a 60-second window per trigger
+    while True:
+        # Simulate streaming by sending data to Kafka every 5 seconds   
+        if curr_time > time.time() + 60:
+            break
+        try:
+            raw_response = ingest_data_api(data)
+            formatted_data = format_data(raw_response)
+            
+            #stream data using produce.send(topic_name, data_in_binary)
+            producer.send('users_created', serializer(formatted_data))
+        except Exception as e:
+            print(f'An error occurred: {e}')
+
+    producer.flush() # tells producer we are good and to flush any remaining messages in the buffer we are done
+    print("Data streamed to Kafka topic 'user_data'.")  
 
 default_args = {
     'owner': 'airflow',
